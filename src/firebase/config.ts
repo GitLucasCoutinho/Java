3b90405ada48b9dd8de5f2797d1e6d707c13497b
Lucas Coutinho <cucasloutinho@gmail.com>
2025-11-13 16:36:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3028333010-9e90f",
+  "appId": "1:409747412634:web:bf7b8d130d608f21e5f123",
+  "apiKey": "AIzaSyBICmfl2CMHGLviDi1CqirOPklYTCExCyM",
+  "authDomain": "studio-3028333010-9e90f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "409747412634"
+};
